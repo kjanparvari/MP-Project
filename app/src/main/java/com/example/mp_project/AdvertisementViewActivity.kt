5bcf3dd11e4ebd7c80db8cc9dd52d56adb58a8e6
Mohammad Tollabi <mohammad.t.0290@gmail.com>
2021-12-08 13:47:21 +0330

    fix: AdvertisementView file name fixed

--- app/src/main/java/com/example/mp_project/AdvertisementViewActivity.kt
+++ app/src/main/java/com/example/mp_project/AdvertisementViewActivity.kt
@@ -3,7 +3,7 @@ package com.example.mp_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AdvertisementView : AppCompatActivity() {
+class AdvertisementViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_advertisement_view)
